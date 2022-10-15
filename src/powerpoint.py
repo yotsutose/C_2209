272ff3390c7add3806068d7a12ff719cb8b4ed84
{kaneko10} <{jinziyounan886@gmail.com}>
2022-10-15 16:44:03 +0900

    ２段

--- src/powerpoint.py
+++ src/powerpoint.py
@@ -1,24 +1,45 @@
 import collections.abc # インポートしないとエラーが発生する
-#from pptx import Presentation # プレゼンテーションを作成
 from pptx.util import Inches  # インチ
-#from pptx.enum.text import PP_ALIGN  # 中央揃えにする用
-#from pptx.util import Cm, Pt # センチ、ポイント
 import pptx
+
+from pptx.enum.shapes import MSO_SHAPE
+from pptx.dml.color import RGBColor
+from pptx.util import Cm,Pt               # 単位指定をするクラス(センチメートル, ポイント単位)
+#from pptx.enum.text import PP_ALIGN  # 中央揃えにする用
+#from pptx import Presentation # プレゼンテーションを作成
 #import os
 
 #cd = os.getcwd()
 prs = pptx.Presentation()
-slide = prs.slides.add_slide(prs.slide_layouts[6])
+prs.slide_width = Inches(11.69) #A4サイズ
+prs.slide_height = Inches(8.27)
+slide = prs.slides.add_slide(prs.slide_layouts[6])  # 空白のスライドを追加
 
-for i in range(1,5):
-    width = 1
-    pic_left = Inches(width)
-    pic_top = Inches(2)
 
-    pic_height = Inches(3)
 
-    image_path = "./input/IMG_000.png"
-    slide.shapes.add_picture(image_path, pic_left, pic_top, height=pic_height)
+start_height = 0.5
+
+for i in range(2):
+    start_left = 1
+    for j in range(4):
+        pic_left = Cm(start_left)
+        pic_top = Cm(start_height)
+
+        pic_height = Cm(9.5)
+
+        image_path = f'./input/IMG_{j:03d}.png'
+        slide.shapes.add_picture(image_path, pic_left, pic_top, height=pic_height)
+
+        rect0 = slide.shapes.add_shape(		# shapeオブジェクト➀を追加
+        MSO_SHAPE.RIGHT_ARROW,   	                    # 図形の種類を[丸角四角形]に指定
+        Cm(3), Cm(2),               # 挿入位置の指定　左上の座標の指定
+        Cm(5), Cm(3))               # 挿入図形の幅と高さの指定
+
+        start_left += 7
+    
+    start_height += 10
+
+
 
 
 prs.save("./test.pptx")
