094a63a815378195467cb16ec68fc26d474edcaf
yotsutose <becchi0424@gmail.com>
2022-10-15 19:30:37 +0900

    import tqdm

--- src/sakabe.py
+++ src/sakabe.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from tqdm import tqdm
 
 def func1():
   cap = cv2.VideoCapture('input/input1.MP4')
@@ -7,7 +8,7 @@ def func1():
   height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
   frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
   print(f'{width=} {height=} {frame_count=}')
-  while(cap.isOpened()):
+  for _ in tqdm(range(0, frame_count)):
     ret, img = cap.read()
     if ret == False:
       break
@@ -15,13 +16,13 @@ def func1():
     # img = cv2.resize(img, dsize=None, fx=10, fy=10)
     cv2.imshow("Video", img)
     if cv2.waitKey(1) & 0xFF == ord('q'): 
-        break
+      break
   cap.release()
   cv2.destroyAllWindows()
 
 
 def main():
-    func1()
+  func1()
 
 if __name__ == '__main__':
 	main()
