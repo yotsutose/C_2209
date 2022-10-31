c769b7976fcebeac5b0b636bb0e87a6ad5be4f89
yotsutose <becchi0424@gmail.com>
2022-10-31 23:09:36 +0900

    コメントの追加

---
+++ WebApp/index.js
@@ -0,0 +1,137 @@
+const fileInput = document.getElementById('filename');
+const video = document.getElementById('video');
+
+// FileInputのchangeイベントで呼び出す関数
+const handleFileSelect = () => {
+    var URL = URL || webkitURL;
+    let videofile = fileInput.files[0];
+    video.src = URL.createObjectURL(videofile);
+    document.getElementById('filename').innerHTML = video.src;
+}
+// ファイル選択時にhandleFileSelectを発火
+fileInput.addEventListener('change', handleFileSelect);
+
+// opencv.jsの読み込みが終わってから動く関数
+function onCvLoaded() {
+    console.log('cv', cv);
+    cv.onRuntimeInitialized = onReady;
+}
+
+// videoの再生時に処理を行う関数達
+let streaming = false;
+function onReady() {
+    console.log('ready');
+    let src;
+    let diff_src;
+    let pre_src;
+    let cap;
+    let index = 0;
+    
+    video.controls = true;
+    video.willReadFrequently = true;
+
+    // videoタグに対して、再生・ポーズ・終了などのアクションに対して発火する関数をセットしている
+    video.addEventListener('play', start);
+    video.addEventListener('pause', pause);
+    video.addEventListener('ended', stop);
+
+    // 動画の再生時に発火する関数
+    // processvideoを最後に呼んでいる
+    function start() {
+        console.log('playing...');
+        streaming = true;
+        const width = video.width;
+        const height = video.height;
+        video.playbackRate = 1.0;
+        src = new cv.Mat(height, width, cv.CV_8UC4);
+        diff_src = new cv.Mat(height, width, cv.CV_8UC4);
+        pre_src = new cv.Mat(height, width, cv.CV_8UC4);
+        cap = new cv.VideoCapture(video);
+        setTimeout(processVideo, 0);
+    }
+
+    // 動画のポーズ時に発火する関数
+    function pause() {
+        // video.play();
+        stop();
+    }
+
+    // 動画の終了時に発火する関数
+    function stop() {
+        console.log('paused or ended');
+        streaming = false;
+    }
+
+    // 再生されている動画から画像を切り出す関数
+    function processVideo() {
+        if (!streaming) {
+            src.delete();
+            diff_src.delete();
+            return; // ストリーミング=falseなら終了
+        }
+        
+        // 今videoで流れている画像をsrcにreadする処理
+        cap.read(src);
+        
+        // diffをとる
+        cv.absdiff(pre_src, src, diff_src);
+        cv.bitwise_not(diff_src, diff_src);
+
+        // ここでdiffから類似度を計算する
+        // todo
+
+        // 選択されたフレームをキャンバスに追加
+        if(index%60==0){ // 「ここを類似度がXXXなら追加する」みたいに書き換える (今の処理は30FPSだから2秒に1回くらい選択)
+            canvas_id = addCanvas(index);
+            cv.imshow(canvas_id, src);
+        }
+
+        // debug用のキャンバス表示 なくても困らない
+        cv.imshow('canvasOutput', diff_src);
+        cv.imshow('canvasOutput2', pre_src);
+        cv.imshow('canvasOutput3', src);
+        
+        index++;
+        pre_src = src.clone();
+        setTimeout(processVideo, 0);
+    }
+}
+
+// 「選択された画像の一覧画面」のところに<canvas>を追加する処理
+function addCanvas(index) {
+    let parentnode = document.getElementsByClassName('canvases');
+    let child = document.createElement('canvas');
+    child.id = "canvas" + (index/60);
+    parentnode[0].appendChild(child);
+    return child.id;
+}
+
+// パワーポイントを作る関数
+function makePPTX() {
+    // 1. パワポの作成、設定
+    let pptx = new PptxGenJS();
+    pptx.defineLayout({ name:'A4', width:11.7, height:8.3 });
+    pptx.layout = 'A4';
+
+    // 2. スライドの追加
+    let slide = pptx.addSlide();
+
+    // 3. こんな感じでスライドにオブジェクトを追加できる
+    slide.addText("Hello World from PptxGenJS...", {
+        x: 1.5,
+        y: 1.5,
+        color: "363636",
+        fill: { color: "F1F1F1" },
+        align: pptx.AlignH.center,
+    });
+
+    // canvasに書かれたデータを読み取るコード
+    cvs = document.getElementById('canvasOutput2');
+    ctx = cvs.getContext('2d');
+    imagedata = cvs.toDataURL("image/jpeg");
+    // 3, 画像データをパワポに追加するメソッドを使う
+    slide.addImage({ data: imagedata, w: 2, h: 4, x: 2, y: 1 });
+
+    // 4. パワポを保存する
+    pptx.writeFile({ fileName: "らくらくトリセツ.pptx" });
+}
