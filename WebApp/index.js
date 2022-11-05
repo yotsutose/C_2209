56e01a5925e7e12e225d53a0906dab701f2e6876
{kaneko10} <{jinziyounan886@gmail.com}>
2022-11-06 00:34:04 +0900

    選択した画像のみ貼るように変更

--- WebApp/index.js
+++ WebApp/index.js
@@ -264,6 +264,7 @@ function makePPTX() {
 
 // PDFを作る関数
 function makePDF() {
+    console.log(stateOfFrame);
     const { jsPDF } = window.jspdf;
     const doc = new jsPDF({orientation: "landscape"}); // 向きを指定する
 
@@ -275,31 +276,37 @@ function makePDF() {
     let width = 43.9;
     let height = 95;
 
+    // let first_img = true
+    let img_index = 0
+
     // フォントサイズの指定
     doc.setFontSize(27);
 
     // canvasに書かれたデータを読み取るコード
-    for(let i=0; i<index; i++) {
-        cvs = document.getElementById(`canvas${i}`);
-        ctx = cvs.getContext('2d');
-        imagedata = cvs.toDataURL("image/jpeg");
-        
-        if (i % 8 === 0 && i !== 0){
-            // ページを増やす
-            doc.addPage({orientation: "landscape"});
-            y = 5;
-        }
+    for(let i=0; i<stateOfFrame.length; i++) {
+        if(stateOfFrame[i]) {
+            cvs = document.getElementById(`canvas${i}`);
+            ctx = cvs.getContext('2d');
+            imagedata = cvs.toDataURL("image/jpeg");
+            
+            if (img_index % 8 === 0 && img_index !== 0){
+                // ページを増やす
+                doc.addPage({orientation: "landscape"});
+                y = 5;
+            }
 
-        // doc.addImage('images/black.png', 'PNG', x-0.6, y-0.6, width+1.2, height+1.2);  // 画像の枠線用の黒画像を先に貼る
-        doc.addImage(imagedata, 'JPEG', x, y, width, height);
-        // 画像番号の追加
-        doc.text(String(i+1), x-13, y+10);
+            // doc.addImage('images/black.png', 'PNG', x-0.6, y-0.6, width+1.2, height+1.2);  // 画像の枠線用の黒画像を先に貼る
+            doc.addImage(imagedata, 'JPEG', x, y, width, height);
+            // 画像番号の追加
+            doc.text(String(img_index+1), x-13, y+10);
 
-        if (i % 4 === 3){
-            x = 25;
-            y += 100;
-        } else {
-            x += 70;
+            if (img_index % 4 === 3){
+                x = 25;
+                y += 100;
+            } else {
+                x += 70;
+            }
+            img_index += 1;
         }
     }
 
@@ -308,38 +315,42 @@ function makePDF() {
     width = 73.9;
     let pre_imagedata = null;
     y = 25;
+    img_index = 0
 
     // フォントサイズの指定
     doc.setFontSize(35);  
 
-    for(let i =0; i<index; i++) {
-        // canvasに書かれたデータを読み取るコード
-        cvs = document.getElementById(`canvas${i}`);
-        ctx = cvs.getContext('2d');
-        imagedata = cvs.toDataURL("image/jpeg");
-        if (i==0) {
+    for(let i =0; i<stateOfFrame.length; i++) {
+        if(stateOfFrame[i]) {
+            // canvasに書かれたデータを読み取るコード
+            cvs = document.getElementById(`canvas${i}`);
+            ctx = cvs.getContext('2d');
+            imagedata = cvs.toDataURL("image/jpeg");
+            if (img_index === 0) {
+                pre_imagedata = imagedata;
+                img_index += 1;
+                continue
+            }
+
+            //ページを増やす
+            doc.addPage({orientation: "landscape"});
+
+            // 左の画像
+            x = ( 297/2 - width ) / 2;
+            // doc.addImage('images/black.png', 'PNG', x-0.8, y-0.8, width+1.6, height+1.6);  // 画像の枠線用の黒画像を先に貼る
+            doc.addImage(pre_imagedata, 'JPEG', x, y, width, height);
+            doc.text(String(img_index), x-15, y+10);
+
+            // 右の画像
+            x += 297/2
+            // doc.addImage('images/black.png', 'PNG', x-0.8, y-0.8, width+1.6, height+1.6);  // 画像の枠線用の黒画像を先に貼る
+            doc.addImage(imagedata, 'JPEG', x, y, width, height);
+            doc.text(String(img_index+1), x-15, y+10);
+
             pre_imagedata = imagedata;
-            continue
+            img_index += 1;
         }
-
-         //ページを増やす
-         doc.addPage({orientation: "landscape"});
-
-        // 左の画像
-        x = ( 297/2 - width ) / 2;
-        // doc.addImage('images/black.png', 'PNG', x-0.8, y-0.8, width+1.6, height+1.6);  // 画像の枠線用の黒画像を先に貼る
-        doc.addImage(pre_imagedata, 'JPEG', x, y, width, height);
-        doc.text(String(i), x-15, y+10);
-
-        // 右の画像
-        x += 297/2
-        // doc.addImage('images/black.png', 'PNG', x-0.8, y-0.8, width+1.6, height+1.6);  // 画像の枠線用の黒画像を先に貼る
-        doc.addImage(imagedata, 'JPEG', x, y, width, height);
-        doc.text(String(i+1), x-15, y+10);
-
-        pre_imagedata = imagedata;
     }
-
     
     // doc.addImage('images/arrow_big.jpg', 'JPEG', 100, 100, 80, 160);
 
@@ -351,6 +362,6 @@ function makePDF() {
     // https://artskydj.github.io/jsPDF/docs/module-addImage.html
     // doc.addImage(imagedata, 'JPEG', 30, 30, 80, 160);
 
-
+    
     doc.save("らくらくトリセツ.pdf");
 }
